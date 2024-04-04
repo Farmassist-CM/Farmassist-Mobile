0dd644c71c704a48e5586657c37b8d799a2d02e0
danieljou <danielsitepro@gmail.com>
2024-04-04 12:59:15 +0100

    BarCode

--- src/screens/ScanBarCode.tsx
+++ src/screens/ScanBarCode.tsx
@@ -1,19 +1,22 @@
 /** @format */
 
 import { VStack, View } from "native-base";
-import React, { useEffect } from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Text } from "react-native";
 
 import { BarCodeScanningResult, Camera, CameraType } from "expo-camera";
 const ScanBarCode = () => {
 	const [permission, requestPermission] = Camera.useCameraPermissions();
 	console.log("permission", permission?.status);
-
+	const [canCapture, setCanCapture] = useState<boolean>(true);
 	useEffect(() => {
 		requestPermission();
 	}, []);
 	const handleCannned = (b: BarCodeScanningResult) => {
-		console.log(b.data);
+		if (canCapture) {
+			Alert.alert("BarCode", b.data);
+			setCanCapture(false);
+		}
 	};
 
 	return (
